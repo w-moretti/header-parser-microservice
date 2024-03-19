669fdedf5fd6c45a4469d27a47bc9e7826702dc2
William Moretti <maleck3@hotmail.com>
2024-03-19 15:51:11 +0000

    Changes to pass all tests

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  //console.log(req.headers);
+  res.json({
+    'ipaddress': req.header('x-forwarded-for'),
+    'language': req.header('accept-language'),
+    'software': req.header('user-agent')
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
